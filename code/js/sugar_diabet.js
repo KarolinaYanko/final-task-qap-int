ebf10df922a19a69aacf47f52af32e8463832479
CarolineYanko <kaya@qapint.com>
2014-01-07 18:52:51 +0200

    add layout for tablets slide

--- code/js/sugar_diabet.js
+++ code/js/sugar_diabet.js
@@ -2,9 +2,7 @@
 	'use strict';
 	document.addEventListener('DOMContentLoaded', function(){
 		setTimeout(function(){
-			document.getElementsByTagName('article')[0].classList.add('animated');
-			document.getElementsByTagName('article')[1].classList.add('animated');
-
+			document.getElementsByTagName('article')[2].classList.add('animated');
 		}, 200);
 	});
 })();
