5815705307b6c12f09eba9133901d37fc7897b86
CarolineYanko <kaya@qapint.com>
2014-01-07 23:22:02 +0200

    wip menu and thumbs

--- code/js/sugar_diabet.js
+++ code/js/sugar_diabet.js
@@ -1,8 +1,13 @@
 (function(){
 	'use strict';
 	document.addEventListener('DOMContentLoaded', function(){
+		var menu = document.getElementById('main-menu'),
+			openMenu = document.getElementsByClassName('menu-open')[0];
+		openMenu.addEventListener('click', function(){
+			menu.classList.add('active');
+		});
 		setTimeout(function(){
-			document.getElementsByTagName('article')[2].classList.add('animated');
+			document.getElementsByTagName('article')[0].classList.add('animated');
 		}, 200);
 	});
 })();
