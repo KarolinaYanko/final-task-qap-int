c75ebb5c0fa85d9af1070158200e8f2ca994f721
CarolineYanko <kaya@qapint.com>
2014-01-07 02:40:35 +0200

    wip ukraine slide

--- code/js/sugar_diabet.js
+++ code/js/sugar_diabet.js
@@ -3,6 +3,8 @@
 	document.addEventListener('DOMContentLoaded', function(){
 		setTimeout(function(){
 			document.getElementsByTagName('article')[0].classList.add('animated');
+			document.getElementsByTagName('article')[1].classList.add('animated');
+
 		}, 200);
 	});
 })();
