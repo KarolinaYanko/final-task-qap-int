e02d58f3327353ba2d3ff873ca823d70fa64c89a
CarolineYanko <kaya@qapint.com>
2014-01-08 21:01:38 +0200

    commit with bugs
    .slideWrap Error

--- code/js/main.js
+++ code/js/main.js
@@ -1,12 +1,21 @@
 (function(){
 	'use strict';
+	var customEvents = ['swipeleft', 'swiperight','slideEnter','slideLeave'],
+	slides = ['sugar_diabet', 'the_prevalence_of_diabetes', 'tablets'];
 	document.addEventListener('DOMContentLoaded', function(){
+		var move = document.getElementsByClassName('move')[0],
+			slideWrap = document.getElementsByClassName('slideWrap')[0];
+		move.addEventListener('click', function(){
+			console.log(1);
+			slideWrap.classList.toggle('second');
+			document.getElementsByTagName('article')[1].classList.add('animated');
+		}, false);
 		setTimeout(function(){
 			document.getElementsByTagName('article')[0].classList.add('animated');
 		}, 200);
-		// ['tap', 'hold', 'swipeleft', 'swiperight', 'swipeup', 'swipedown'].forEach(function(eventName){
-		// 	customEvents[eventName] = document.createEvent('UIEvents');
-		// 	customEvents[eventName].initEvent(eventName, true, true);
-		// }
+		customEvents.forEach(function(eventName){
+			customEvents[eventName] = document.createEvent('UIEvents');
+			customEvents[eventName].initEvent(eventName, true, true);
+		});
 	});
 })();
