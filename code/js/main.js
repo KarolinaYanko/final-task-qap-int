0ca6eae16100896c5ffda1ea7e209e7fe664fdcd
CarolineYanko <kaya@qapint.com>
2014-01-09 00:03:12 +0200

    add thumb's switching

--- code/js/main.js
+++ code/js/main.js
@@ -1,21 +1,12 @@
 (function(){
 	'use strict';
-	var customEvents = ['swipeleft', 'swiperight','slideEnter','slideLeave'],
-	slides = ['sugar_diabet', 'the_prevalence_of_diabetes', 'tablets'];
+
 	document.addEventListener('DOMContentLoaded', function(){
-		var move = document.getElementsByClassName('move')[0],
-			slideWrap = document.getElementsByClassName('slideWrap')[0];
-		move.addEventListener('click', function(){
-			console.log(1);
-			slideWrap.classList.toggle('second');
-			document.getElementsByTagName('article')[1].classList.add('animated');
-		}, false);
+
 		setTimeout(function(){
-			document.getElementsByTagName('article')[0].classList.add('animated');
-		}, 200);
-		customEvents.forEach(function(eventName){
-			customEvents[eventName] = document.createEvent('UIEvents');
-			customEvents[eventName].initEvent(eventName, true, true);
-		});
+			var p = document.getElementById('presentation').classList.add('first');
+
+		}, 150);
+
 	});
 })();
--- code/js/menu.js
+++ code/js/menu.js
@@ -1,8 +1,38 @@
 (function(){
 	'use strict';
 	var menu = document.getElementById('main-menu'),
-		openMenu = document.getElementsByClassName('menu-open')[0];
+		openMenu = document.getElementsByClassName('menu-open')[0],
+		sugar_thumb = document.getElementsByClassName('menu-thumb')[0],
+		ukraine_thumb = document.getElementsByClassName('menu-thumb')[1],
+		tablets_thumb = document.getElementsByClassName('menu-thumb')[2],
+		presentation = document.getElementById('presentation'),
+		i = document.getElementById('indicators');
+
 	openMenu.addEventListener('click', function(){
-		menu.classList.toggle('active');
+		menu.classList.add('active');
 	},false);
+
+	sugar_thumb.addEventListener('click', function(){
+		presentation.classList.add('first');
+		presentation.classList.remove('second');
+		presentation.classList.remove('third');
+		menu.classList.remove('active');
+		i.children[0].checked = true;
+	}, false);
+
+	ukraine_thumb.addEventListener('click', function(){
+		presentation.classList.remove('first');
+		presentation.classList.remove('third');
+		presentation.classList.add('second');
+		menu.classList.remove('active');
+		i.children[1].checked = true;
+	}, false);
+
+	tablets_thumb.addEventListener('click', function(){
+		presentation.classList.remove('first');
+		presentation.classList.remove('second');
+		presentation.classList.add('third');
+		menu.classList.remove('active');
+		i.children[2].checked = true;
+	}, false);
 })();
